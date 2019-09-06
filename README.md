280c10790473481da8c1923d38e26ddd82986308
Chris Kuika <chriskuika12@gmail.com>
2019-09-06 14:08:48 +0100

    initialization

---
+++ README.md
@@ -0,0 +1,244 @@
+## flashy.js jQuery plugin
+
+> The package requires jQuery in your html document.
+
+```html
+<html>
+ <head>
+  <title> Hello flashy </title>
+ </head>
+ <body>
+  <script src="jquery/jquery.min.js"> </script>
+ </body>
+</html>
+```
+
+### Start by first cloning the ** Githud ** project in your application.
+
+once downloaded you must include the following files:
+
+```
+css/
+ - flashy.min.css
+ - flashy.css
+js/
+ - flashy.min.js
+ - flashy.js
+```
+
+> in the production please include the files minifiers not to overload the web page.
+
+```html
+<html>
+ <head>
+  <title> Hello flashy </title>
+  <link rel="stylesheet" type="text/css" href="css/flashy.min.css">
+ </head>
+ <body>
+  <script src="jquery/jquery.min.js"> </script>
+  <script src="js/flashy.min.js"> </script>
+ </body>
+</html>
+```
+
+### Good start practice :)
+
+After inclusion of flashy.min.js file or flashy.js you have a ```flashy``` variable that interacts directly with the package.
+
+Here's how to say hello to your visitors each time you load the web page.
+
+```html
+<html>
+ <head>
+  <title> Hello flashy </title>
+  <link rel="stylesheet" type="text/css" href="css/flashy.min.css">
+ </head>
+ <body>
+  <script src="jquery/jquery.min.js"> </script>
+  <script src="js/flashy.min.js"> </script>
+  <script>
+  flashy('Hello I'm flashy');
+  </script>
+ </body>
+</html>
+```
+
+> Try it!
+
+## 1 Colors
+
+you want to change the appearance of flashy? then pass an object in second parameter as mentioned below:
+
+```javascript
+<script>
+ flashy('Hello I'm flashy', {
+    type: 'flashy__info'
+ });
+</script>
+```
+
+here are the types of effects available:
+
+ - flashy__info
+ - flashy__danger
+ - flashy__success
+ - flashy__warning
+
+> Try it!
+
+
+You have certainly understood that the effect types above are only ** CSS ** classes and therefore you have full possibility to add your types :)
+
+## 2 Animation
+
+Want to change the flashy animation? oh yes the package accepts ```animate.css```
+
+in this case, please download and add ** Animate.css ** in your html document
+
+```html
+<html>
+ <head>
+  <title> Hello flashy </title>
+  <link rel="stylesheet" type="text/css" href="css/flashy.min.css">
+  <link rel="stylesheet" type="text/css" href="animate.css">
+ </head>
+  ...
+</html>
+```
+
+So :
+
+```javascript
+<script>
+ flashy('Hello I'm flashy', {
+    animatedIn: 'animated wobble'//default bounceInRight
+    animatedOut: 'animated rubberBand'//default bounceOutRight
+ });
+</script>
+```
+
+> Try it!
+
+
+## 3 ESC key
+
+Try to launch flashy in your browser then click on the ** ESC ** button!
+
+you can disable it as below:
+
+```javascript
+<script>
+ flashy('Hello I'm flashy', {
+   quitESC: false
+ });
+</script>
+```
+
+> Try it!
+
+
+## 4 Closing Icon
+
+You can change the icon as below:
+
+```javascript
+<script>
+ flashy('Hello I'm flashy', {
+   icon: '<i class="fa fa-close"> </i>
+ });
+</script>
+```
+
+
+## 5 Time
+
+Time is counted in mileseconde:
+
+```javascript
+<script>
+ flashy('Hello I'm flashy', {
+   timeout: 10000//default 5000 is 5s
+ });
+</script>
+```
+
+
+## 6 Class
+
+Want to add extra CSS classes during the flashy release?
+
+```javascript
+<script>
+ flashy('Hello I'm flashy', {
+   addClass: "classname classname classmane"
+ });
+</script>
+```
+
+
+## 7 Stop
+
+Do you want to cancel the flashy time so that flashy does not go away after its release?
+
+```javascript
+<script>
+ flashy('Hello I'm flashy', {
+   stop: true
+ });
+</script>
+```
+
+
+## 8 CallBack
+
+Add an anonymous function that will be called at each flashy shutdown.
+
+```javascript
+<script>
+ flashy('Hello I'm flashy', function () {
+ alert('Flashy completed');
+ });
+</script>
+```
+
+# Current use
+
+You can pass arguments to ** flashy ** in different ways:
+
+* Pass a title to flashy
+
+```javascript
+<script>
+ flashy('Hello I'm flashy');
+</script>
+```
+
+```javascript
+<script>
+ flashy({title: 'Hello I'm flashy'});
+</script>
+```
+
+These two codes above do exactly the same thing.
+
+* Here are some more examples
+
+```javascript
+<script>
+ flashy('Hello I'm flashy', {stop: true}, function () {
+   alert('Thanks flashy');
+ });
+</script>
+```
+
+```javascript
+<script>
+ flashy({title: 'Ah, my flashy', stop: true}, function () {
+   alert('Thanks flashy');
+ });
+</script>
+```
+
+
+
+All your concerns at chriskuika12@gmail.com or search me on Facebook **ZonkedCode**
---
+++ js/flashy.js
@@ -0,0 +1,283 @@
+/*!
+ * flashy.js
+ * Version: 0.4.0
+ *
+ * With flashy flash for subscribers calmly
+ * ----------------------------------------
+ * Copyright 2019 Flashy.js Contributors
+ *
+ * @author zonkedCode <chriskuika12@gmail.com>
+ */
+
+;(function(global, flashy){
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = flashy() :
+    typeof define === 'function' && define.amd ? define(flashy) :
+    global.flashy = flashy();
+}(this, function(){
+
+	"use strict";
+
+	// We check if jQuery is indefinite in the document Object 
+	if(typeof window.jQuery === 'undefined' && window.$ === 'undefined') throw new Error('flashy required Jquery')
+
+	/**
+	 * @var string
+	 */
+	const FLASHY_OBJ = 'object';
+
+	/**
+	 * @var string
+	 */
+	const FLASHY_FUNCTION = 'function';
+
+	/**
+	 * @var string
+	 */
+	const FLASHY_STRING = 'string';
+
+
+	/**
+	 * @var object
+	 */
+	var Flashy = {};
+
+	/**
+	 * @var function
+	 */
+	var FlashyCallBack = function() {};
+
+	/**
+	 * @var nullable
+	 */
+	var AnimatedProgress = null;
+
+
+
+	Flashy.Version = "0.4.0";
+
+	Flashy.OPTIONS = {
+		title : "Hello flashy !",
+		animatedIn : "animated bounceInRight",
+		animatedOut : "animated bounceOutRight",
+		quitESC : true,
+		icon : "<i>x</i>",
+		timeout : 5000,
+		type : "flashy__danger",
+		addClass : null,
+		stop : false
+	};
+
+	Flashy.OPTIONS_COPY = {};
+	Flashy.OPTIONS_REPLACE = {};
+
+	/**
+	 * The body of the package inserted into the DOM.
+	 */
+	Flashy.NODE_PRESENT = function() {
+		$('body').append('<div class="flashy" flashy id="flashy"><div class="flashy__container"><div class="flashy__body"></div><div class="flashy__icon"></div></div></div>');
+	}();
+
+	Flashy.ClassName = { 
+		bodyClass : ".flashy__body",
+		coreClass : ".flashy",
+		iconClass : ".flashy__icon",
+		hideClass : ".flashy__hide",
+		display : ".flashy__show",
+		iconChild : ".flashy__icon i",
+		activeClass : ".flashy__active",
+		active : "flashy__active"
+	};
+
+	Flashy.Show = function() {
+		this
+			.Hide()
+			.OptionPreliminary();
+
+		$(this.ClassName.bodyClass).html(Flashy.OPTIONS_COPY.title);
+		$(this.ClassName.iconClass).html(Flashy.OPTIONS_COPY.icon);
+
+		$(this.ClassName.coreClass)
+
+			.css({display: "inline", right : "1%"})
+			.addClass(Flashy.OPTIONS_COPY.type)
+			.addClass(Flashy.OPTIONS_COPY.addClass)
+			.addClass(Flashy.OPTIONS_COPY.animatedIn)
+			.addClass(Flashy.ClassName.active);
+
+			Flashy.Timer();
+
+		return this;
+	}
+	
+	/**
+	 * Latency of the package.
+	 */ 
+	Flashy.Timer = function() {
+		if (! Flashy.OPTIONS_COPY.stop) {
+			AnimatedProgress = setTimeout(function(){	 
+				Flashy.Out();
+				ApplyCallBack();
+
+			}, Flashy.OPTIONS_COPY.timeout);
+		}
+
+		Flashy.OPTIONS_REPLACE = {};
+	}
+
+	function checkAnimatedProgressIsClear() {
+		if (AnimatedProgress) {
+			clearTimeout(AnimatedProgress);
+		}
+	}
+	
+	/**
+	 * Ensure animation in the core "Out".
+	 */
+	Flashy.AnimatedIsOut = function() {
+		$(this.ClassName.coreClass)
+			.removeClass(Flashy.OPTIONS_COPY.animatedIn)
+
+			.addClass(this.OPTIONS_COPY.animatedOut);
+	}
+
+	Flashy.AnimatedClass = {
+		Animated : 'animated',
+		AnimatedBounceRight : 'bounceOutRight',
+		AnimatedInRight : 'bounceInRight'
+	}; 
+
+	function StringifyOptions() {
+		Flashy.OPTIONS_COPY = JSON.parse(JSON.stringify(Flashy.OPTIONS));
+	}
+
+	/**
+	 * Add the parameters of the user by the parameters of defect.
+	 */ 
+	function CompactParams(params) {
+		StringifyOptions();
+
+		for (var id in params) {
+			if (id in Flashy.OPTIONS) {			
+				Flashy.OPTIONS_COPY[id] = params[id];
+			}
+		}
+	}
+
+	Flashy.OptionPreliminary = function() {
+		
+		// One test to check if the parameters defect are already the variable
+		// "OPTIONS_COPY" if not let us add we them.
+		for (var id in Flashy.OPTIONS) {
+			if (typeof Flashy.OPTIONS_COPY[id] == "undefined") {
+				Flashy.OPTIONS_COPY[id] = Flashy.OPTIONS[id];
+			}	
+		}
+
+		for (var id in Flashy.OPTIONS_REPLACE) {
+			Flashy.OPTIONS_COPY[id] = Flashy.OPTIONS_REPLACE[id];
+		}
+	}
+
+	Flashy.Out = function() {
+		checkAnimatedProgressIsClear();
+		Flashy.AnimatedIsOut();	
+	}
+
+	Flashy.Hide = function() {
+
+		if (this.isActive()) {
+			this.Out();		
+
+			var Core = $(Flashy.ClassName.coreClass);
+
+				Core
+				.attr("style", "")
+				.removeClass(Flashy.OPTIONS_COPY.type)
+				.removeClass(Flashy.OPTIONS_COPY.addClass)
+				.removeClass(Flashy.OPTIONS_COPY.animatedIn)
+				.removeClass(Flashy.OPTIONS_COPY.animatedOut)
+				.removeClass(Flashy.ClassName.active)
+				.fadeOut();	
+
+			for(var id in Flashy.AnimatedClass) {
+				if (Core
+					.hasClass(Flashy.AnimatedClass[id])) {
+					Core.removeClass(Flashy.AnimatedClass[id]);
+
+				}
+			}
+		}
+
+		return this;
+	}
+
+	Flashy.isActive = function() {
+		return $(Flashy.ClassName.coreClass).hasClass(Flashy.ClassName.active);
+	}
+
+	// This event attent that the user types on the key 
+	// escape for called the function related to this last
+
+	Flashy.KeyUp = function() {
+		$(document).on('keyup', function(e) {
+			if(e.which === 27 && Flashy.OPTIONS_COPY.quitESC) {
+				Flashy.Out();
+			}
+		});
+
+		return this;
+	}
+
+	Flashy.Quit = function() {
+		$(document).on('click', Flashy.ClassName.iconChild, function(e) {
+			Flashy.Out();
+		});
+
+		return this;
+	}
+
+	function ApplyCallBack() {
+		if (typeof FlashyCallBack === "function" && FlashyCallBack) {
+			FlashyCallBack();
+		} 
+	}
+
+	var Plugin = function(title, params, $callBack){
+
+		if (typeof title == "undefined" && typeof params == "undefined" && typeof $callBack == "undefined") {
+			throw new Error("you must specify at least a parameter");
+		}
+
+		/*
+		| We want to return below the "Plugin" elegant allowing a facility of use 
+		| this is why all the conditions allow to check to them various parameters which 
+		| the user can transmit to the package. since the "Javascript" manage not the optional  
+		| parameters, it is thus we are to counter to make all these probabilities of checking. 
+		*/
+
+		if (typeof title == FLASHY_STRING) {
+			Flashy.OPTIONS_REPLACE['title'] = title;
+		} else 
+		if (typeof title == FLASHY_OBJ) {
+			CompactParams(title);
+		} 
+		if (typeof params == FLASHY_OBJ) {
+			CompactParams(params);
+		} else 
+		if (typeof params == FLASHY_FUNCTION) {
+			FlashyCallBack = params;
+		}   
+		if (typeof $callBack == FLASHY_FUNCTION) {
+			FlashyCallBack = $callBack;
+		}
+
+		Flashy
+			.Show()
+			.KeyUp()
+			.Quit();			
+	}
+
+	return Plugin;
+
+}));
+
